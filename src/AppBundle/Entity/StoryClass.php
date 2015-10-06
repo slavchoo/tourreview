0cbf3bca27c94cf47cc19bc2b3f9157a8e577c89
MShapira <shapiro.mihail.anatolevich@gmail.com>
2015-10-06 23:25:32 +0300

    metadata changes

--- src/AppBundle/Entity/StoryClass.php
+++ src/AppBundle/Entity/StoryClass.php
@@ -3,12 +3,43 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="storyClass")
+ */
+
 class StoryClass
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $user_id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $title;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
     protected $creation_date;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
     protected $description;
+
+    /**
+     * @ORM\Column(type="text")
+     */
     protected $image;
+
 }
--- src/AppBundle/Entity/UserClass.php
+++ src/AppBundle/Entity/UserClass.php
@@ -2,17 +2,69 @@
 
 namespace AppBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="storyClass")
+ */
+
 class UserClass
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $username;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     protected $password;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $email;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
     protected $first_name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     protected $last_name;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     protected $personal_data;
+
+    /**
+     * @ORM\Column(type="text")
+     */
     protected $phone;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     protected $location;
+
+    /**
+     * @ORM\Column(type="string")
+     */
     protected $photo;
     protected $registration_date;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
 }

e7df654eef10dbcdafba2ecb4ff6279e522755fe
MShapira <shapiro.mihail.anatolevich@gmail.com>
2015-10-12 16:35:05 +0300

    FormBuilder for Article page

--- src/AppBundle/Controller/ArticlePageController.php
+++ src/AppBundle/Controller/ArticlePageController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Form\ArtcleType;
 
 class ArticlePageController
 {
@@ -12,4 +13,10 @@ class ArticlePageController
         return $this->render('default/articles.html.twig');
     }
 
+    public function newAction(Request $request)
+    {
+        $article = new Article();
+        $form = $this->createForm(new ArticleType(), $article);
+        $form->add('submit');
+    }
 }
--- src/AppBundle/Form/ArticleType.php
+++ src/AppBundle/Form/ArticleType.php
@@ -6,27 +6,28 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class PostType extends AbstractType
+class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('summary', 'textarea')
-            ->add('description', 'textarea')
+            ->add('title', 'text')
+            ->add('summary', 'text')
+            ->add('description', 'text')
             ->add('creation_date', 'datetime')
+            ->add('image', 'image')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Post'
+            'data_class' => 'AppBundle\Entity\Article'
         ));
     }
 
     public function getName()
     {
-        return 'post';
+        return 'article';
     }
 }
